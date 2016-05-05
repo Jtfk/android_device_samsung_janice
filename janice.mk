2b5216faed1f3ec732322e56ef5a11d8ef21d7b5
Epirex <estebanguzzo@gmail.com>
2016-05-05 19:49:46 -0300

    Disable genfstab for now

--- janice.mk
+++ janice.mk
@@ -8,8 +8,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
 # F2FS
-PRODUCT_PACKAGES += \
-    genfstab
+#PRODUCT_PACKAGES += \
+#    genfstab
 
 # STE
 PRODUCT_COPY_FILES += \
@@ -33,6 +33,7 @@ $(call inherit-product, device/common/gps/gps_eu_supl.mk)
 # Init files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/device_tunables.rc:root/device_tunables.rc \
+    $(LOCAL_PATH)/rootdir/fstab.samsungjanice:root/fstab.samsungjanice \
     $(LOCAL_PATH)/rootdir/init.samsungjanice.rc:root/init.samsungjanice.rc \
     $(LOCAL_PATH)/rootdir/init.recovery.samsungjanice.rc:root/init.recovery.samsungjanice.rc \
     $(LOCAL_PATH)/rootdir/ueventd.samsungjanice.rc:root/ueventd.samsungjanice.rc
