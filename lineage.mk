69b03333289e65bdcc5ad9d95311731841eebead
Epirex <estebanguzzo@gmail.com>
2017-09-11 03:35:42 -0300

    Move to lineage

--- lineage.mk
+++ lineage.mk
@@ -5,15 +5,15 @@ PRODUCT_RELEASE_NAME := GT-I9070
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/janice/full_janice.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := janice
-PRODUCT_NAME := cm_janice
+PRODUCT_NAME := lineage_janice
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I9070
 PRODUCT_MANUFACTURER := samsung
